67e072cf34d267445efff558bca20b5164f9695a
ybuiko <juliabuiko@gmail.com>
2024-04-02 20:48:49 -0400

    adding hosts

--- patientsmanagement/settings.py
+++ patientsmanagement/settings.py
@@ -33,6 +33,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#right below the ALLOW_HOST  variable add this.
+## Handling Allowed Hosts on Render
+## adds the render.com hostname to be in ALLOWED_HOSTS
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
